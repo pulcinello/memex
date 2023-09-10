c9c507c4213534d850303edb1fb64c83530e3d1b
nello <watshon@gmail.com>
2023-09-10 20:54:59 +0800

    add favicon and prettify some code

--- logic/lightbox.js
+++ logic/lightbox.js
@@ -1,51 +1,48 @@
-function Lightbox()
-{
+function Lightbox() {
   this.container = null;
   this.img = null;
 
-  this.install = function(container, prefix)
-  {
+  this.install = function (container, prefix) {
     this.container = container;
-    
-    let back = document.createElement('div');
-    back.className = prefix + '-back';
-    this.addEvent(back, 'click', function(){ lightbox.close(); });
+
+    let back = document.createElement("div");
+    back.className = prefix + "-back";
+    this.addEvent(back, "click", function () {
+      lightbox.close();
+    });
     this.container.appendChild(back);
 
-    this.img = document.createElement('img');
-    this.img.className = prefix + '-img';
-    this.addEvent(this.img, 'click', function(){ lightbox.close(); });
+    this.img = document.createElement("img");
+    this.img.className = prefix + "-img";
+    this.addEvent(this.img, "click", function () {
+      lightbox.close();
+    });
     this.container.appendChild(this.img);
-  }
+  };
 
-  this.load = function(file)
-  {
+  this.load = function (file) {
     this.img.src = file;
-  	this.container.style.display = 'block';
-  }
+    this.container.style.display = "block";
+  };
 
-  this.close = function()
-  {
-  	if (this.container.style.display != 'none')
-  	{
-  		this.container.style.display = 'none';
-  	}
-  }
-
-  this.handle = function(element, file)
-  {
-    this.addEvent(element, 'click', function(){ lightbox.load(file); });
-  }
-
-  this.addEvent = function(element, evnt, funct)
-  {
-    if (element.attachEvent)
-    {
-      return element.attachEvent('on'+evnt, funct);
+  this.close = function () {
+    if (this.container.style.display != "none") {
+      this.container.style.display = "none";
     }
-    else
-    {
+  };
+
+  this.handle = function (element, file) {
+    this.addEvent(element, "click", function () {
+      lightbox.load(file);
+    });
+  };
+
+  this.addEvent = function (element, evnt, funct) {
+    if (element.attachEvent) {
+      return element.attachEvent("on" + evnt, funct);
+    } else {
       return element.addEventListener(evnt, funct, false);
     }
-  }
+  };
 }
+
--- logic/wrap.js
+++ logic/wrap.js
@@ -1,40 +1,30 @@
-function Wrap()
-{
-  this.start = function(data)
-  {
-    this.commaSplit = function(data)
-    {
-      if (data !== undefined)
-      {
+function Wrap() {
+  this.start = function (data) {
+    this.commaSplit = function (data) {
+      if (data !== undefined) {
         var result = data.split(",");
-        for (var c = 0; c < result.length; c++)
-        {
+        for (var c = 0; c < result.length; c++) {
           result[c] = result[c].trim().toLowerCase();
         }
         return result;
       }
-      return data;  
-    }
+      return data;
+    };
 
-    this.objectSplit = function(data)
-    {
-      if (typeof data == 'object')
-      {
-        for (let o = 0; o < data.length; o++)
-        {
-          if (data[o].substr(0,2) == '> ')
-          {
-            data[o] = data[o].substr(2,data[o].length-1);
+    this.objectSplit = function (data) {
+      if (typeof data == "object") {
+        for (let o = 0; o < data.length; o++) {
+          if (data[o].substr(0, 2) == "> ") {
+            data[o] = data[o].substr(2, data[o].length - 1);
           }
         }
       }
       return data;
-    }
+    };
 
     let database = new Indental(data).parse();
     let keys = Object.keys(database);
-    for (let i = 0; i < keys.length; i++)
-    {
+    for (let i = 0; i < keys.length; i++) {
       let entry = database[keys[i]];
 
       entry.AUTH = this.commaSplit(entry.AUTH);
@@ -49,128 +39,88 @@ function Wrap()
     }
 
     return database;
-  }
+  };
 
-  this.filter = function(target, db)
-  {
+  this.filter = function (target, db) {
     let tempDatabase = {};
-    if (target == '')
-    {
+    if (target == "") {
       tempDatabase = db;
-    }
-    else
-    {
+    } else {
       let keys = Object.keys(db);
-      if (target == 'term')
-      {
-        for (let i = 0; i < keys.length; i++)
-        {
+      if (target == "term") {
+        for (let i = 0; i < keys.length; i++) {
           let value = db[keys[i]];
-          if (typeof value.TERM !== 'undefined')
-          {
+          if (typeof value.TERM !== "undefined") {
             tempDatabase[keys[i]] = db[keys[i]];
           }
         }
-      }
-      else
-      {
+      } else {
         var splitTarget = target.split("-");
-        if (splitTarget[0] == 'tag')
-        {
+        if (splitTarget[0] == "tag") {
           // TAG
           let tagRequest = decodeURI(splitTarget[1]);
-          for (let i = 0; i < keys.length; i++) 
-          {
+          for (let i = 0; i < keys.length; i++) {
             let value = db[keys[i]];
-            if (typeof value.TAGS !== 'undefined')
-            {
-              for (let t = 0; t < value.TAGS.length; t++)
-              {
-                if (value.TAGS[t] == tagRequest)
-                {
+            if (typeof value.TAGS !== "undefined") {
+              for (let t = 0; t < value.TAGS.length; t++) {
+                if (value.TAGS[t] == tagRequest) {
                   tempDatabase[keys[i]] = db[keys[i]];
                 }
               }
             }
           }
         }
-        if (splitTarget[0] == 'proj')
-        {
+        if (splitTarget[0] == "proj") {
           // PROJECT
           let projectRequest = decodeURI(splitTarget[1]);
-          for (let i = 0; i < keys.length; i++) 
-          { 
+          for (let i = 0; i < keys.length; i++) {
             let value = db[keys[i]];
-            if (typeof value.PROJ !== 'undefined')
-            {
-              for (let p = 0; p < value.PROJ.length; p++)
-              {
-                if (value.PROJ[p] == projectRequest)
-                {
+            if (typeof value.PROJ !== "undefined") {
+              for (let p = 0; p < value.PROJ.length; p++) {
+                if (value.PROJ[p] == projectRequest) {
                   tempDatabase[keys[i]] = db[keys[i]];
                 }
               }
             }
           }
-        }
-        else if (splitTarget[0] == 'type')
-        {
+        } else if (splitTarget[0] == "type") {
           // TYPE
           let typeRequest = decodeURI(splitTarget[1]);
-          for (let i = 0; i < keys.length; i++) 
-          { 
+          for (let i = 0; i < keys.length; i++) {
             let value = db[keys[i]];
-            if (typeof value.TYPE !== 'undefined')
-            {
-              if (typeof value.TYPE == 'object')
-              {
+            if (typeof value.TYPE !== "undefined") {
+              if (typeof value.TYPE == "object") {
                 // This entry has multiple types
-                for (let t = 0; t < value.TYPE.length; t++)
-                {
-                  if (value.TYPE[t] == typeRequest)
-                  {
+                for (let t = 0; t < value.TYPE.length; t++) {
+                  if (value.TYPE[t] == typeRequest) {
                     tempDatabase[keys[i]] = db[keys[i]];
                   }
                 }
-              }
-              else
-              {
+              } else {
                 // This entry has a single type
-                if (value.TYPE == typeRequest)
-                {
+                if (value.TYPE == typeRequest) {
                   tempDatabase[keys[i]] = db[keys[i]];
                 }
               }
             }
           }
-        }
-        else if (splitTarget[0] == 'done')
-        {
+        } else if (splitTarget[0] == "done") {
           // DONE
           let doneValue = decodeURI(splitTarget[1]);
-          for (let i = 0; i < keys.length; i++) 
-          { 
+          for (let i = 0; i < keys.length; i++) {
             let value = db[keys[i]];
-            if (doneValue == 'true')
-            {
+            if (doneValue == "true") {
               // true
-              if (typeof value.DONE !== 'undefined')
-              {
-                if (value.DONE == 'true')
-                {
+              if (typeof value.DONE !== "undefined") {
+                if (value.DONE == "true") {
                   tempDatabase[keys[i]] = db[keys[i]];
                 }
               }
-            }
-            else
-            {
+            } else {
               // false
-              if (typeof value.DONE === 'undefined')
-              {
+              if (typeof value.DONE === "undefined") {
                 tempDatabase[keys[i]] = db[keys[i]];
-              }
-              else if (value.DONE == 'false')
-              {
+              } else if (value.DONE == "false") {
                 tempDatabase[keys[i]] = db[keys[i]];
               }
             }
@@ -179,60 +129,45 @@ function Wrap()
       }
     }
     return tempDatabase;
-  }
+  };
 
-  this.stats = function(db)
-  {
+  this.stats = function (db) {
     // CALCULATE
     let dbKeys = Object.keys(db);
-    var stats = 
-    {
+    var stats = {
       total: dbKeys.length,
       types: {},
       tags: {},
       terms: 0,
-      done: 0
+      done: 0,
     };
 
-    for (var i = 0; i < dbKeys.length; i++)
-    {
+    for (var i = 0; i < dbKeys.length; i++) {
       // TYPE
-      if (typeof db[dbKeys[i]].TYPE !== 'undefined')
-      {
-        if (typeof stats.types[db[dbKeys[i]].TYPE] !== 'undefined')
-        {
-          stats.types[db[dbKeys[i]].TYPE] ++;
-        }
-        else
-        {
+      if (typeof db[dbKeys[i]].TYPE !== "undefined") {
+        if (typeof stats.types[db[dbKeys[i]].TYPE] !== "undefined") {
+          stats.types[db[dbKeys[i]].TYPE]++;
+        } else {
           stats.types[db[dbKeys[i]].TYPE] = 1;
         }
       }
 
       // TAGS
-      if (typeof db[dbKeys[i]].TAGS !== 'undefined')
-      {
-        for (var t = 0; t < db[dbKeys[i]].TAGS.length; t++) 
-        {
-          if (typeof stats.tags[db[dbKeys[i]].TAGS[t]] !== 'undefined')
-          {
-            stats.tags[db[dbKeys[i]].TAGS[t]] ++;
-          }
-          else
-          {
+      if (typeof db[dbKeys[i]].TAGS !== "undefined") {
+        for (var t = 0; t < db[dbKeys[i]].TAGS.length; t++) {
+          if (typeof stats.tags[db[dbKeys[i]].TAGS[t]] !== "undefined") {
+            stats.tags[db[dbKeys[i]].TAGS[t]]++;
+          } else {
             stats.tags[db[dbKeys[i]].TAGS[t]] = 1;
           }
         }
       }
 
       // TERM
-      if (typeof db[dbKeys[i]].TERM !== 'undefined')
-      {
+      if (typeof db[dbKeys[i]].TERM !== "undefined") {
         let count = 0;
-        for (var t = 0; t < db[dbKeys[i]].TERM.length; t++)
-        {
-          if (db[dbKeys[i]].TERM[t].substr(0,2) == '> ')
-          {
+        for (var t = 0; t < db[dbKeys[i]].TERM.length; t++) {
+          if (db[dbKeys[i]].TERM[t].substr(0, 2) == "> ") {
             count++;
           }
         }
@@ -241,41 +176,36 @@ function Wrap()
       }
 
       // DONE
-      if (typeof db[dbKeys[i]].DONE !== 'undefined')
-      {
-        if (db[dbKeys[i]].DONE == 'true')
-        {
-          stats.done ++;
-        } 
+      if (typeof db[dbKeys[i]].DONE !== "undefined") {
+        if (db[dbKeys[i]].DONE == "true") {
+          stats.done++;
+        }
       }
-    } 
+    }
 
     // SORT TYPES, TAKE TOP X
     // Create items array
-    var typeItems = Object.keys(stats.types).map(function(key) 
-    {
+    var typeItems = Object.keys(stats.types).map(function (key) {
       return [key, stats.types[key]];
     });
     // Sort the array based on the second element
-    typeItems.sort(function(first, second) 
-    {
+    typeItems.sort(function (first, second) {
       return second[1] - first[1];
     });
     stats.types = typeItems;
 
     // SORT TAGS, TAKE TOP X
     // Create items array
-    var tagItems = Object.keys(stats.tags).map(function(key) 
-    {
+    var tagItems = Object.keys(stats.tags).map(function (key) {
       return [key, stats.tags[key]];
     });
     // Sort the array based on the second element
-    tagItems.sort(function(first, second) 
-    {
+    tagItems.sort(function (first, second) {
       return second[1] - first[1];
     });
     stats.tags = tagItems;
 
     return stats;
-  }
+  };
 }
+
